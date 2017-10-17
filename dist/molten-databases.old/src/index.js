5c3d449cffc2891b3f19f6aa8aef255d23bff5e9
Jack Farley <or4e3dsw@bytes.nz>
2017-10-17 08:12:15 +0300

    Add read() options for sorting and limiting
    - Add read() options for sorting and limiting result
    - Move dist files into root of project
    - Fix github url to project

--- dist/molten-databases.old/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var initialisation_spec_1 = require("./tests/initialisation.spec");
-var makeDatabaseTests = function (createDatabase, testCreateDatabaseOptions) {
-    console.log('makeDatabaseTests called');
-    initialisation_spec_1.default(createDatabase, testCreateDatabaseOptions);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = makeDatabaseTests;
